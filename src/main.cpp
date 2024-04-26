cf39415b4d51cf768854b673c582ddad5ce66176
Arief Nur Misuari <arief.nur@efishery.com>
2024-04-26 19:00:23 +0800

    feart(main): adding CurrentDetection implementation

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,44 @@
 // #include "miscellaneous/I2C_Scanner.h"
+// #include "miscellaneous/emonlib_test.h"
+// #include "miscellaneous/ADS1115_emon.h"
+
 #include <Arduino.h>
 #include <WiFi.h>
 
-#include <interfaces/BTInterface.h>
-#include <interfaces/AlarmInterface.h>
-#include <interfaces/DisplayInterface.h>
-#include <interfaces/StorageInterface.h>
-#include <interfaces/CurrentMeterInterface.h>
+#include <CurrentDetection.h>
+#include <drivers/esp32/CurrentMeter_ADS.h>
 
-class SettingsModule
-{
-    bool load(std::string settingFile);
-    bool store(std::string settingFile);
-    bool setInterval(uint32_t milliseconds);
-    uint32_t getInterval();
-    bool setTimezone(int8_t timezone);
-    int8_t getTimezone();
-};
+// TODO: Add autoformatter for a consistent coding style.
+// Feature proposal:
+// * Settings store to the storage including timezone info.
+// and create timeFormatter(uint32_t unixtime) to follow timezone, for unit
+// testing purpose. Edge cases: +- UTC, UTC+24 +36,
+// * Download OpenWeather JSON and parse, for unit testing.
+// * Settings in JSON, for unit testing.
+// * Configuration via UART (or via WebUI for paid content), can be unit tested
+// *
+CurrentMeter_ADS currentMeter;
 
-class Formatter
-{
-    std::string formatTime(uint32_t unixtime, int8_t timezone);
-};
+CurrentDetection cd(currentMeter, &Serial);
 
-void setup()
-{
+uint32_t meterLastUpdated = 0;
+
+void setup() {
+  Serial.begin(115200);
+
+  cd.init();
+
+  Serial.println("Current Detection Initialized.");
+
+  meterLastUpdated = millis();
 }
 
-void loop()
-{
+void loop() {
+//   if (millis() - meterLastUpdated >= ws._cfg.getInterval()) {
+  if (millis() - meterLastUpdated >= 500U) {
+    meterLastUpdated = millis();
+    cd.updateMeterData();
+  }
+
+//   ws.checkSerialConfig();
 }
